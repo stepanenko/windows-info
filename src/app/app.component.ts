8f8459807ecaf4e8e48d9faffba1f65c7976b869
Serhiy Stepanenko <serhiy.stepanenko@aiesec.net>
2018-12-07 15:19:26 +0200

    server and angular client created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ChatService } from './chat.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-socket-io';
+export class AppComponent implements OnInit {
+
+  constructor(private chat: ChatService) {}
+
+  ngOnInit() {
+    this.chat.messages.subscribe(msg => {
+      console.log(msg);
+    });
+  }
+
+  sendMessage() {
+    this.chat.sendMsg('Test message');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ChatService } from './chat.service';
+import { WebsocketService } from './websocket.service';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    ChatService,
+    WebsocketService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
